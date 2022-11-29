6f8101904540093386842ede9fc8ffc944b85811
Igor Garcia <igorgg@gmail.com>
2022-11-29 16:49:02 -0300

    feat: contact CRUD ok

--- backend/src/modules/contacts/contacts.controller.ts
+++ backend/src/modules/contacts/contacts.controller.ts
@@ -1,5 +1,14 @@
-import { Body, Controller, HttpCode, Post } from '@nestjs/common';
-import { createContactDTO } from './contacts.dto';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { createContactDTO, updateContactDTO } from './contacts.dto';
 import { ContactsService } from './contacts.service';
 
 @Controller('contacts')
@@ -10,5 +19,29 @@ export class ContactsController {
   @HttpCode(201)
   async create(@Body() data: createContactDTO) {
     return this.contactsService.create(data);
+  }
+
+  @Get()
+  async listContacts() {
+    return this.contactsService.listContacts();
+  }
+
+  @Get(':contact_id')
+  async listContact(@Param('contact_id') contact_id: string) {
+    return this.contactsService.listContact(contact_id);
+  }
+
+  @Patch(':contact_id')
+  async updateContact(
+    @Param('contact_id') contact_id: string,
+    @Body() data: updateContactDTO,
+  ) {
+    return this.contactsService.updateContact(data, contact_id);
+  }
+
+  @Delete(':contact_id')
+  @HttpCode(204)
+  async deleteContact(@Param('contact_id') contact_id: string) {
+    return this.contactsService.deleteContact(contact_id);
   }
 }
--- backend/src/modules/contacts/contacts.service.ts
+++ backend/src/modules/contacts/contacts.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { createContactDTO } from './contacts.dto';
+import { createContactDTO, updateContactDTO } from './contacts.dto';
 
 @Injectable()
 export class ContactsService {
@@ -20,5 +20,81 @@ export class ContactsService {
     const contact = await this.prisma.contact.create({ data });
 
     return contact;
+  }
+
+  async listContacts() {
+    const contacts = await this.prisma.contact.findMany({
+      include: {
+        client: true,
+      },
+    });
+
+    return contacts;
+  }
+
+  async listContact(contact_id: string) {
+    const contact = await this.prisma.contact.findUnique({
+      where: {
+        id: contact_id,
+      },
+      include: {
+        client: true,
+      },
+    });
+
+    if (!contact) {
+      throw new HttpException('Contact not found', HttpStatus.NOT_FOUND);
+    }
+
+    return contact;
+  }
+
+  async updateContact(data: updateContactDTO, contact_id: string) {
+    const contact = await this.prisma.contact.findUnique({
+      where: {
+        id: contact_id,
+      },
+    });
+
+    if (!contact) {
+      throw new HttpException('Contact not found', HttpStatus.NOT_FOUND);
+    }
+    if (data.clientId) {
+      const client = await this.prisma.client.findUnique({
+        where: {
+          id: data.clientId,
+        },
+      });
+      if (!client) {
+        throw new HttpException('Client not found', HttpStatus.NOT_FOUND);
+      }
+    }
+
+    const updateContact = await this.prisma.contact.update({
+      data,
+      where: {
+        id: contact_id,
+      },
+    });
+
+    return updateContact;
+  }
+
+  async deleteContact(contact_id: string) {
+    const contact = await this.prisma.contact.findUnique({
+      where: {
+        id: contact_id,
+      },
+    });
+
+    if (!contact) {
+      throw new HttpException('Contact not found', HttpStatus.NOT_FOUND);
+    }
+
+    await this.prisma.contact.delete({
+      where: {
+        id: contact_id,
+      },
+    });
   }
 }
